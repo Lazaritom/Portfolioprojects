a479bdcfd00616612cbde9ad87bc632face25f5e
Lazaritom <113486331+Lazaritom@users.noreply.github.com>
2023-08-30 11:34:34 -0700

    minor

--- Procrasinot/src/App.jsx
+++ Procrasinot/src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.css'
-import './App'
+import './App.css'
+
+
 export default function App() {
   
   return (
     <>
     <h1 className='text-center'>Procrasinot</h1>
-      <div className="container border border-secondary row center">
-        <div className="container border border-primary col-lg">shcisa</div>
-        <div className='container border border-success col-lg'>asfjahs</div>
-        <div className='container border border-success col-lg'>asfjasfjsa</div>
+      <div className="container border border-secondary d-flex justify-content-center">
+        <div className="container border border-primary col-lg ">shcisa</div>
+        <div className='container border border-success col-lg '>asfjahs</div>
+        <div className='container border border-success col-lg '>asfjasfjsa</div>
       </div>
     </>
   )
