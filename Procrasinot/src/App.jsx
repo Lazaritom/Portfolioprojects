0bd6f5b08a542e57553e4f3312f4b4c0c81e9793
Lazaritom <113486331+Lazaritom@users.noreply.github.com>
2023-08-31 13:30:02 -0700

    Added timer (faulty)
    Timer pops up but doesn't rerender for now. needs a fix

--- Procrasinot/src/App.jsx
+++ Procrasinot/src/App.jsx
@@ -2,23 +2,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
-import TimerChoice from './Components'
+import TimerChoice,{Timer} from './Components'
 import { Timer1, Timer2, Session } from './data'
 
 export default function App() {
   return (
     <>
       <h1 className='text-center'>Procrasinot</h1>
-      <div className="container border border-secondary d-flex justify-content-center">
+      <div className="container d-flex justify-content-center">
           <div className="container border border-primary col-lg ">
             <h1>Study Timers</h1>
             <TimerChoice name={Timer1.name} goal={Timer1.goal} time={Timer1.time}/>
             <TimerChoice name={Timer2.name} goal={Timer2.goal} time={Timer2.time}/>
             <TimerChoice name={Session.name} goal={Session.goal} time={Session.time}/> 
-            /*need to create inputs here to save into object */
+            {/*need to create inputs here to save into object */}
           </div>
           <div className='container border border-success col-lg '>
             <h1>Timer</h1>
+            <Timer />
           </div>
           <div className='container border border-success col-lg '>
             <h1>History</h1>
--- Procrasinot/src/components.jsx
+++ Procrasinot/src/components.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function TimerChoice({name, goal, time}) {
     return (
@@ -10,7 +10,46 @@ export default function TimerChoice({name, goal, time}) {
         </div>
     )
 }
-
-export function Timer() {
-    
-}
+// NEEDS A FIX> DOESN"T WORK DONT FORGET FIX FIX FIX
+export const Timer = (time) => {
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+  
+    const TotalMinutes = 50; // Set the time in minutes
+  
+    const calcTime = (remainingTime) => {
+      const remainingSeconds = Math.floor((remainingTime / 1000) % 60);
+      const remainingMinutes = Math.floor((remainingTime / 1000 / 60) % 60);
+      const remainingHours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
+  
+      setHours(remainingHours);
+      setMinutes(remainingMinutes);
+      setSeconds(remainingSeconds);
+    };
+  
+    useEffect(() => {
+      const TimerInterval = setInterval(() => {
+        const now = Date.now();
+        const endTime = now + (TotalMinutes * 60 * 1000); // Calculate the end time in milliseconds
+  
+        if (now < endTime) {
+          const remainingTime = endTime - now;
+          calcTime(remainingTime);
+        } else {
+          clearInterval(TimerInterval);
+          setHours(0);
+          setMinutes(0);
+          setSeconds(0);
+        }
+      }, 1000);
+  
+      return () => clearInterval(TimerInterval); // Cleanup of interval after unmounting
+    }, [TotalMinutes]);
+  
+    return (
+      <div className="timer">
+           <h1>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</h1>
+      </div>
+    );
+  };
