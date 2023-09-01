ce4be2d3fe017685d362916b74a0eb4cb69e3d94
Lazaritom <113486331+Lazaritom@users.noreply.github.com>
2023-08-31 22:00:36 -0700

    TODO update
    1. gave the divs id's in app.jsx
    2. wrote in notes future plans

--- Procrasinot/src/App.jsx
+++ Procrasinot/src/App.jsx
@@ -15,21 +15,21 @@ export default function App() {
     <>
       <h1 className='text-center'>Procrasinot</h1>
       <div className="container d-flex justify-content-center">
-          <div className="container border border-primary col-lg ">
+          <div className="container border border-primary col-lg" id="Study_timers">
             <h1>Study Timers</h1>
             <TimerChoice object={Timer1}/><button onClick={() => setShowMyComp1(!showMyComp1)}>Add this Procrasinoter/Delete this Procrasinoter</button>
             <TimerChoice object={Timer2}/><button onClick={() => setShowMyComp2(!showMyComp2)}>Add this Procrasinoter/Delete this Procrasinoter</button>
             <TimerChoice object={Session}/><button onClick={() => setShowMyComp3(!showMyComp3)}>Needs work, doesn't do anything yet. make templete for user</button>
             {/*need to create inputs here to save into object */}
           </div>
-          <div className='container border border-success col-lg '>
+          <div className='container border border-success col-lg' id="Timer">
             <h1>Timer</h1>
             <TimerCountDown targetTimeInSeconds={1800} />
             {showMyComp1 && <TimerChoice object={Timer1} />}
             {showMyComp2 && <TimerChoice object={Timer2} />}
             {showMyComp3 }{/**add a template for user to creat timer */}
           </div>
-          <div className='container border border-success col-lg '>
+          <div className='container border border-success col-lg' id="History">
             <h1>History</h1>
           </div>
         </div>
