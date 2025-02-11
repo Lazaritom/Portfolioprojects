df0770b41c23c01cfb1e22a9e98f9ce396e2c075
Lazaritom <113486331+Lazaritom@users.noreply.github.com>
2025-02-11 13:53:05 -0800

    Updating code to use new language and framework updates.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolioprojects
-projects
---
+++ my-app/src/data.js
@@ -0,0 +1,15 @@
+export const Timer1 = {
+    'name': "Short-session",
+    "time": [30,5,2],
+    "goal": "Best use of study time If time is limited to under two hours. For eveyr 30 minutes of studying, you are allowed 5 minutes of rest to make the concepts sink in."
+}
+export const Timer2 = {
+    'name': "Long-session",
+    "time": [60,15,2],
+    "goal": "Best study timer for periods of 2-2.5 hours. For every 1 hour of studying, you get 15 minutes to allow your brain to rest and the knowledge to solidify in your memory."
+}
+export const Session = {
+    'name': "Create a personalized Procrasinotor",
+    "time": ["Write any amount of time with breaks that you want",], //inside an array because of the TIMERCHOICE component in ./components
+    "goal": "Write any goal that you want to work on or accomplish within any time limit that you set."
+    }
